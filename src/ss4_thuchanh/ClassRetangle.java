6e7bbfc0081fa04fe87218ed50830af0f42ef9bc
3K3ki <danghung090@gmail.com>
2023-03-09 14:02:14 +0700

    first commit

---
+++ src/ss4_thuchanh/ClassRetangle.java
@@ -0,0 +1,25 @@
+package ss4_thuchanh;
+
+public class ClassRetangle {
+    double width, height;
+
+    public ClassRetangle() {
+    }
+
+    public ClassRetangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea() {
+        return this.width * this.height;
+    }
+
+    public double getPerimeter() {
+        return (this.width + this.height) * 2;
+    }
+
+    public String display() {
+        return "Rectangle{" + "width=" + width + ", height=" + height + "}";
+    }
+}
---
+++ src/ss4_thuchanh/Main.java
@@ -0,0 +1,16 @@
+package ss4_thuchanh;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the width:");
+        double width = scanner.nextDouble();
+        System.out.print("Enter the height:");
+        double height = scanner.nextDouble();
+        ClassRetangle rectangle = new ClassRetangle(width, height);
+        System.out.println("Your Rectangle \n"+ rectangle.display());
+        System.out.println("Perimeter of the Rectangle: "+ rectangle.getPerimeter());
+        System.out.println("Area of the Rectangle: "+ rectangle.getArea());
+    }
+}
